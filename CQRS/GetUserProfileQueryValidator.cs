0dcd1db762633c396df267c53f126a861bdd8e6b
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-26 13:14:31 +0200

    Fix validator

--- CQRS/GetUserProfileQueryValidator.cs
+++ CQRS/GetUserProfileQueryValidator.cs
@@ -4,6 +4,6 @@ public class GetUserProfileQueryValidator : AbstractValidator<GetUserProfileQuer
 {
     public GetUserProfileQueryValidator()
     {
-        RuleFor(x => x.UserId).NotNull().NotEmpty();
+        RuleFor(x => x.CognitoUserName).NotNull().NotEmpty();
     }
 }
